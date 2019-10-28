35eb8acebfa5be2f40bf2bd786bbab1a6336a8c8
张开 <zhangkai@192.168.1.5>
2019-10-28 23:56:20 +0800

    add sortStack

---
+++ sortStack.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/karalabe/cookiejar.v2/collections/stack"
+)
+
+func main() {
+	var originStack = stack.New()
+	var sortStack = stack.New()
+	originStack.Push(1)
+	originStack.Push(6)
+	originStack.Push(7)
+	originStack.Push(3)
+	sortStack.Push(originStack.Pop())
+	for {
+		if originStack.Size() == 0 {
+			break
+		}
+		var tmp = originStack.Pop()
+		for {
+			if !sortStack.Empty() && tmp.(int) > sortStack.Top().(int) {
+				originStack.Push(sortStack.Pop())
+			} else {
+				break
+			}
+		}
+		sortStack.Push(tmp)
+	}
+
+	for {
+		if sortStack.Size() == 0 {
+			break
+		}
+		fmt.Println(sortStack.Pop())
+	}
+}
