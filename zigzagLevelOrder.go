3650f073f2a7f4ccbeda285c8bcc7538c979b092
needkane <e.zl@qq.com>
2019-11-28 22:25:27 -0500

    update

--- zigzagLevelOrder.go
+++ zigzagLevelOrder.go
@@ -38,11 +38,72 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+type simpleStack []*TreeNode
 
-func zigzagLevelOrder(root *TreeNode) [][]int {
-
+func (s simpleStack) Push(v *TreeNode) simpleStack {
+	return append(s, v)
+}
+func (s simpleStack) Pop() (simpleStack, *TreeNode) {
+	l := len(s)
+	if l == 0 {
+		return s, nil
+	}
+	node := s[l-1]
+	return s[:l-1], node
+}
+func (s simpleStack) IsEmpty() bool {
+	if len(s) == 0 {
+		return true
+	} else {
+		return false
+	}
 }
 
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	var levels [][]int
+	if root == nil {
+		return nil
+	}
+	var stack_odd, stack_even simpleStack
+	stack_even = stack_even.Push(root)
+	level := 0
+	for {
+		if stack_odd.IsEmpty() && stack_even.IsEmpty() {
+			break
+		}
+		// start the current level
+		levels = append(levels, []int{})
+		if level%2 == 0 {
+			level_length := len(stack_even)
+			for i := 0; i < level_length; i++ {
+				var node *TreeNode
+				stack_even, node = stack_even.Pop()
+				levels[level] = append(levels[level], node.Val)
+				if node.Left != nil {
+					stack_odd = stack_odd.Push(node.Left)
+				}
+				if node.Right != nil {
+					stack_odd = stack_odd.Push(node.Right)
+				}
+			}
+		} else {
+			level_length := len(stack_odd)
+			for i := 0; i < level_length; i++ {
+				var node *TreeNode
+				stack_odd, node = stack_odd.Pop()
+				levels[level] = append(levels[level], node.Val)
+				if node.Right != nil {
+					stack_even = stack_even.Push(node.Right)
+				}
+				if node.Left != nil {
+					stack_even = stack_even.Push(node.Left)
+				}
+			}
+		}
+		level++
+	}
+	return levels
+}
 func createRoot(datas []int) (root *TreeNode) {
 
 	if len(datas) == 0 || datas[0] == 0 {
